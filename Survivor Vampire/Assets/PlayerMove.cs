c443c9350cd6266ecf4a01cdbab0f8828f9421a8
Woncest <107243849+Woncest@users.noreply.github.com>
2024-04-18 11:04:30 +0200

    Camera + Simple Animations

--- Survivor Vampire/Assets/PlayerMove.cs
+++ Survivor Vampire/Assets/PlayerMove.cs
@@ -7,12 +7,16 @@ public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rgbd2d;
     Vector3 movementVector;
+    [SerializeField] float speed = 3f;
+
+    Animate animate;
 
     // Start is called before the first frame update
     void Start()
     {
         rgbd2d = GetComponent<Rigidbody2D>();
         movementVector = new Vector3();
+        animate = GetComponent<Animate>();
     }
 
     // Update is called once per frame
@@ -20,6 +24,10 @@ public class PlayerMove : MonoBehaviour
     {
         movementVector.x = Input.GetAxisRaw("Horizontal");
         movementVector.y = Input.GetAxisRaw("Vertical");
+
+        animate.horizontal = movementVector.x;
+
+        movementVector *= speed;
 
         rgbd2d.velocity = movementVector;
     }
