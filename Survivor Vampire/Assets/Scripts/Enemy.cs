dbc3e5f3de4d6404cce963edfff4f5e6cccd3872
Woncest <107243849+Woncest@users.noreply.github.com>
2024-04-20 12:35:05 +0200

    First Enemy that stalks you

---
+++ Survivor Vampire/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform targetDestionation;
+    GameObject targetGameObject;
+    [SerializeField] float speed;
+
+    Rigidbody2D rgdbd2d;
+
+    private void Awake(){
+        rgdbd2d = GetComponent<Rigidbody2D>();
+        targetGameObject = targetDestionation.gameObject;
+    }
+
+    private void FixedUpdate(){
+        Vector3 direction = (targetDestionation.position - transform.position).normalized;
+        rgdbd2d.velocity = direction * speed;
+    }
+
+    private void OnCollisionStay2D(Collision2D coll){
+        if(coll.gameObject == targetGameObject){
+            Attack();
+        }
+    }
+
+    private void Attack(){
+        Debug.Log("Attack");
+    }
+}
