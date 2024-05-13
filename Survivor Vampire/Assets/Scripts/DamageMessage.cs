1f1f0c7052ce31f7e1ac2c3c3e8b3b435a4a9859
Woncest <107243849+Woncest@users.noreply.github.com>
2024-05-13 17:27:39 +0200

    Text is floating up

--- Survivor Vampire/Assets/Scripts/DamageMessage.cs
+++ Survivor Vampire/Assets/Scripts/DamageMessage.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,15 +8,26 @@ public class DamageMessage : MonoBehaviour
 {
     [SerializeField] float TimeToLive = 1f;
     float ttl = 1f;
+    public float fadeSpeed = 0.01f;
+    private TextMeshPro text;
 
     private void OnEnable(){
         ttl = TimeToLive;
+        text = GetComponent<TextMeshPro>();
     }
 
     private void Update(){
         ttl -= Time.deltaTime;
+        if(Time.frameCount %18 == 0){
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f , transform.position.z);
+            Color currentColor = text.color;
+            float newAlpha = Mathf.Max(0f, currentColor.a - fadeSpeed);
+            text.color = new Color(currentColor.r, currentColor.g, currentColor.b, newAlpha);
+        }
         if (ttl < 0){
             gameObject.SetActive(false);
+            Color currentColor = text.color;
+            text.color = new Color(currentColor.r, currentColor.g, currentColor.b, 1);
         }
     }
 }
