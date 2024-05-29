511dbb32fd7118bbdd0eddf1630ca9b2c23b9508
Woncest <107243849+Woncest@users.noreply.github.com>
2024-05-29 08:31:53 +0200

    Garlick shows when it would damage now

--- Survivor Vampire/Assets/Scripts/GarlickWeapon.cs
+++ Survivor Vampire/Assets/Scripts/GarlickWeapon.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class GarlickWeapon : WeaponBase
 {
     [SerializeField] float attackAreaSize = 3f;
+    [SerializeField] GameObject radiusCircle;
+
+    private SpriteRenderer radiusCircleRenderer;
+    private float initialAlpha = 0.15f; // 15% alpha
+    private float maxAlpha = 0.3f; // 30% alpha
+
+    void Start()
+    {
+        radiusCircleRenderer = radiusCircle.GetComponent<SpriteRenderer>();
+        Color color = radiusCircleRenderer.color;
+        color.a = initialAlpha;
+        radiusCircleRenderer.color = color;
+    }
 
     public override void Attack()
     {
@@ -16,9 +29,17 @@ public class GarlickWeapon : WeaponBase
 
     public new virtual void Update(){
         timer -= Time.deltaTime;
+        float alpha = Mathf.Lerp(maxAlpha, initialAlpha, timer / weaponStats.timeToAttack);
+        
+        Color color = radiusCircleRenderer.color;
+        color.a = alpha;
+        radiusCircleRenderer.color = color;
         if (timer < 0){
             Attack();
             timer = weaponStats.timeToAttack;
+
+            color.a = initialAlpha;
+            radiusCircleRenderer.color = color;
         }
         transform.position = GameManager.instance.playerTransform.position;
     }
