26c5ed0f21efa7cd4409c30e5cca3d7cec607a03
Woncest <107243849+Woncest@users.noreply.github.com>
2024-05-31 11:44:50 +0200

    Second Fire Ball spawns

--- Survivor Vampire/Assets/FireBall.cs
+++ Survivor Vampire/Assets/FireBall.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class FireBall : WeaponBase
     public float speed = 1.0f;
     private float angle = 0.0f;
     private Collider2D attackTarget;
+    private GameObject secondFireBall;
+    public bool setSecondFireBall = false;
     public override void Attack()
     {
         ApplyDamage(new Collider2D[] { attackTarget });
@@ -24,16 +27,27 @@ public class FireBall : WeaponBase
 
     public new virtual void Update()
     {
+        CheckForExtraFireBalls();
         angle += speed * Time.deltaTime * 1.5f;
 
         float x = Mathf.Cos(angle) * radius;
         float y = Mathf.Sin(angle) * radius;
 
-        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
+        Vector2 directionFireBall = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
 
         transform.localPosition = new Vector3(x, y, transform.localPosition.z);
 
-        float angleDegrees = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angleDegrees = Mathf.Atan2(directionFireBall.y, directionFireBall.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angleDegrees + 90);
+    }
+
+    private void CheckForExtraFireBalls()
+    {
+        if(weaponStats.numberOfAttacks == 2 && !setSecondFireBall){
+            secondFireBall = Instantiate(gameObject);
+            secondFireBall.GetComponent<FireBall>().setSecondFireBall = true;
+            setSecondFireBall = true;
+            secondFireBall.transform.parent = transform.parent;
+        }
     }
 }
